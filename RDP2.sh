c75a27418507ab9bfd6cee784d1237d8794072d5
Carlos Ferreira <kyrvim@gmail.com>
2021-03-26 11:34:19 -0300

    aa
    agora foi

--- RDP2.sh
+++ RDP2.sh
@@ -2,19 +2,26 @@
 printf 'YT Channel - https://www.youtube.com/c/AKYTRC \n'
 printf "RDP installing... " >&2
 {
-sudo useradd -m kyrvim
-sudo adduser kyrvim sudo
-echo 'kyrvim:33646797' | sudo chpasswd
+sudo useradd -m ALOK
+sudo adduser ALOK sudo
+echo 'ALOK:1234' | sudo chpasswd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo DEBIAN_FRONTEND=dialog \
-sudo adduser kyrvim chrome-remote-desktop
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo apt install obs-studio -y
+sudo apt install firefox -y
+sudo apt install falkon -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo adduser ALOK chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nGO to https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - kyrvim -c """$CRP"""
-printf 'kyrvim, u SUDO Password Is 33646797 \n'
+su - ALOK -c """$CRP"""
+printf 'Your SUDO Password Is 1234 \n'

29064925b8920a25ae04491e51206bb632dc1449
Carlos Ferreira <kyrvim@gmail.com>
2021-03-26 11:11:36 -0300

    Update RDP2.sh
    teste

--- RDP2.sh
+++ RDP2.sh
@@ -5,21 +5,11 @@ printf "RDP installing... " >&2
 sudo useradd -m kyrvim
 sudo adduser kyrvim sudo
 echo 'kyrvim:33646797' | sudo chpasswd
-sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=dialog \
-apt install --assume-yes xfce4 desktop-base
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-sudo apt install obs-studio -y
-sudo apt install firefox -y
-sudo apt install falkon -y
-sudo apt install terminator -y
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg --install google-chrome-stable_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
 sudo adduser kyrvim chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Completed " >&2 ||

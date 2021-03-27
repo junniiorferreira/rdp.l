13e3663aa6744aa299af72f9c74166042dd365cc
Carlos Ferreira <kyrvim@gmail.com>
2021-03-26 23:38:03 -0300

    noite
    noite

--- RDP2.sh
+++ RDP2.sh
@@ -2,28 +2,25 @@
 printf 'YT Channel - https://www.youtube.com/c/AKYTRC \n'
 printf "RDP installing... " >&2
 {
-sudo apt-get install xfce4-terminal
-sudo update-alternatives --config x-terminal-emulator
-sudo useradd -m kkkyrvimaa
-sudo adduser kkkyrvimaa chrome-remote-desktop
-sudo adduser kkkyrvimaa sudo
-echo 'kkkyrvimaa:1234' | sudo chpasswd
+sudo useradd -m KYRVIM
+sudo adduser KYRVIM sudo
+echo 'KYRVIM:33646797' | sudo chpasswd
+sed -i '/#!\/bin\/bash/' /etc/passwd
 sudo apt-get update
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
-apt install --assume-yes xfce4 desktop-base
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-sudo apt install obs-studio -y
-sudo apt install firefox -y
-sudo apt install falkon -y
+sudo apt-get upgrade -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=readline apt install --assume-yes xfce4 xfce4-goodies
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo adduser KYRVIM chrome-remote-desktop
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 } &> /dev/null &&
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nGO to https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - kkkyrvimaa -c """$CRP"""
-printf 'Your SUDO Password Is 1234 \n'
+su - KYRVIM -c """$CRP"""
+printf 'Your SUDO Password Is DEFAULT \n'
